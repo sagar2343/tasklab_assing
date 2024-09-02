4e3b83575c65f5ca79b874144708e88007ba1503
sagarhome <tyrant@gmail.com>
2024-09-02 17:05:53 +0530

    second commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -70,7 +70,9 @@ class _LoginScreenState extends State<LoginScreen> {
                       // Password validation: Minimum 6 characters, at least one uppercase letter, one lowercase letter, one number, and one special character
                       final passwordRegex = RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$');
                       if (!passwordRegex.hasMatch(value)) {
-                        return 'Password must be at least 6 characters long, include an uppercase letter, a lowercase letter, a number, and a special character';
+                        return 'Password must be at least 6 characters long,\n'
+                            'include an uppercase letter, a lowercase letter,\n'
+                            'a number, and a special character.';
                       }
                       return null;
                     },
